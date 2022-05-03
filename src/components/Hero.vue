f62f968e5044e0dc7234bf80b16b362bc18d05f7
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-05-03 20:51:21 +0100

    fix: removed typo

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
     <main class="flex flex-col justify-center h-screen h-[80vh] w-full">
-        <p class="font-['Cabinet_Grotesk'] text-white text-5xl md:text-6xl xl:text-7xl max-w-3xl xl:max-w-4xl text-neutral-400 leading-relaxed">Favour is a <br class="md:hidden"> <span class="font-medium text-7xl lg:text-8xl xl:text-9xl text-white"> designer</span> specialising in <span class="font-medium text-7xl lg:text-8xl xl:text-9xl text-white">UI, UX</span> and in <span class="font-medium text-7xl lg:text-8xl xl:text-9xl text-white">Product Design.</span></p>
+        <p class="font-['Cabinet_Grotesk'] text-white text-5xl md:text-6xl xl:text-7xl max-w-3xl xl:max-w-4xl text-neutral-400 leading-relaxed">Favour is a <br class="md:hidden"> <span class="font-medium text-7xl lg:text-8xl xl:text-9xl text-white"> designer</span> specialising in <span class="font-medium text-7xl lg:text-8xl xl:text-9xl text-white">UI, UX</span> and <span class="font-medium text-7xl lg:text-8xl xl:text-9xl text-white">Product Design.</span></p>
     </main>
 </template>
