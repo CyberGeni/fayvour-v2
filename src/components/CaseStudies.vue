a0e47d084b8bedf6845dbc1595bfed0ba81e1e4a
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-05-09 14:21:45 +0100

    refracored case studies

--- src/components/CaseStudies.vue
+++ src/components/CaseStudies.vue
@@ -1,9 +1,9 @@
 <template>
     <main>
-        <h1 class="leading-none text-4xl sm:text-5xl lg:text-7xl stroke-1 transition">selected cases</h1>
+        <h1 class="">my work</h1>
         <section v-for="casestudy in cases" :key="casestudy.id">
-            <img class="w-24 sm:w-32 lg:48" :src="casestudy.image" :alt="casestudy.description" />
-            <router-link :to="casestudy.link" class="text-white">
+            <router-link :to="casestudy.link" class="">
+                <img class="" :src="casestudy.image" :alt="casestudy.description" />
                 <h2>{{casestudy.title}}</h2>
                 <p>{{casestudy.description}}</p>    
             </router-link>
@@ -42,45 +42,12 @@ export default {
 }
 </script>
 <style scoped>
-    main {
-        position: relative;
-        z-index: inherit;
-        margin-top: 3em;
-    }
     h1 {
-        font-family: 'Trap';
         background: -webkit-linear-gradient(180deg, #A8FFBB 16.21%, #6B81F0 41.72%, #CC48ED 81.21%);
         color: rgba(27, 25, 25);
         /* the transparent content with just border thingy */
         -webkit-background-clip: text;
         -webkit-text-stroke: 4px transparent;
-        margin: 0.2em 0;
         width: fit-content;
-    }
-    a {
-        font-family: 'Circular';
-        display: flex;
-        flex-direction: column;
-        align-items: space-evenly;
-        text-decoration: none;
-        color: white
-    }
-    h2 {
-        font-size: 32px;
-        font-weight: 500;
-        margin-bottom: 0.1em;
-    }
-    section {
-        display: flex;
-        align-items: center;
-        margin: 1.5em 0;
-        transition: 350ms cubic-bezier(0, 0.98, 0.25, 1.13);
-    }
-    section:hover {
-        margin-left: 4.5em;
-        transform: scale(1.1);
-    }
-    section img {
-        margin-right: 1.5em;
     }
 </style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,5 @@
 <template>
     <main class="flex flex-col justify-center h-screen h-[75vh] w-full">
-      <div class="absolute truncate top-24 bg-transparent w-full border-y-2 border-neutral-400 ml-[-5%] py-1"> 
-        <p class="scroll text-neutral-400 font-['Supreme']">user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate  • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate  • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate  • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate • user centered design advocate</p>
-      </div>
-      <p class="font-['Cabinet_Grotesk'] text-5xl md:text-6xl xl:text-7xl max-w-3xl xl:max-w-4xl text-neutral-400 leading-relaxed">Favour is a <br class="md:hidden"> <span class="font-medium text-7xl lg:text-8xl xl:text-9xl text-white"> designer</span> specialising in <span class="font-medium text-7xl lg:text-8xl xl:text-9xl text-white">UI, UX</span> and <span class="font-medium text-7xl lg:text-8xl xl:text-9xl text-white">Product Design.</span></p>
-      <div class="hidden sm:block font-['Sentient'] bg-decor absolute right-0 text-[205px] top-[60vh] w-full">
-        <span>creative.</span>
-      </div>
       <div class="absolute top-[85vh]">
         <lottie-player src="https://assets4.lottiefiles.com/packages/lf20_ascfjwtt.json" background="transparent" speed="1" style="width: 50px; height: 50px;" loop autoplay></lottie-player>
       </div>
@@ -14,31 +7,10 @@
     </main>
 </template>
 <style scoped>
-.scroll {
-  display : flex;
-  flex-shrink: 0;
-  height: 50px;
-  align-items: center;
-  animation: slide-left 400s linear infinite;
-  background: none;
-}
-
-  @keyframes slide-left {
-  from {
-    -webkit-transform: translateX(0);
-            transform: translateX(0);
-  }
-  to {
-    -webkit-transform: translateX(-300%);
-            transform: translateX(-300%);
-  }
-}
 .bg-decor {
   width: 100%;
-
-        right: 10px;
-        text-align: right;
-
+  right: 10px;
+  text-align: right;
 }
 .bg-decor span {
         background: -webkit-linear-gradient(180deg, #ffffff 16.21%, #8a8a8a 41.72%, #ffffff 81.21%);
@@ -47,6 +19,5 @@
         -webkit-background-clip: text;
         -webkit-text-stroke: 4px transparent;
         opacity: 0.1;
-
     }
 </style>
