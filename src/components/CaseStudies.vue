8f48e54632dcbc59a2527b715fb02c73df8bcbf2
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-05-09 14:42:37 +0100

    restructured object holding case studies

--- src/components/CaseStudies.vue
+++ src/components/CaseStudies.vue
@@ -16,25 +16,36 @@ export default {
         return {
             cases: [
                 {
-                    image: '/src/assets/risk-free-logo.svg',
-                    title: 'risk free',
-                    description: 'a new way to quit over thinking',
-                    link: 'riskfree',
+                    image: '/src/assets/koyata.svg',
+                    title: 'Koyata',
+                    description: 'A language learning app that utilizes gamification techniques to help people learn Nigerian languages. Kinda like Duolingo, but a tad better. See how.',
+                    link: 'koyata',
+                    tag: 'EDU TECH',
                     id: 1
                 },
                 {
-                    image: '/src/assets/spotify-logo.svg',
-                    title: 'spotify',
-                    description: 'exploring lyrics and karaoke features',
+                    image: '/src/assets/spotify.svg',
+                    title: 'Spotify',
+                    description: 'I did some research and added a lyrics (before Spotify did) and karaoke feature to the platform. Check out my thought process and the results.',
                     link: 'spotify',
+                    tag: 'MEDIA TECH',
                     id: 2
                 },
                 {
-                    image: '/src/assets/smart-ear-logo.svg',
-                    title: 'smart ear',
-                    description: 'coming soon...',
-                    link: 'smart-ear',
+                    image: '/src/assets/daffodil.svg',
+                    title: 'Daffodil',
+                    description: 'I designed a hospital management dashboard. It helps to manage patients, appointments, among other things. Check it out here.',
+                    link: 'daffodil',
+                    tag: 'HEALTH TECH',
                     id: 3
+                },
+                {
+                    image: '/src/assets/riskfree.svg',
+                    title: 'Risk free',
+                    description: 'A study conducted at the University of Michigan revealed that 73% of adults between the ages of 25 and 35 overthink. Check out my solution.',
+                    link: 'riskfree',
+                    tag: 'HEALTH TECH',
+                    id: 4
                 }
             ]
         }
