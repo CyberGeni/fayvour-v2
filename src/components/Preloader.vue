13d041b06dea67fa774603a03ac19d4f78ad9fae
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-05-20 00:43:22 +0100

    styled the dummy loading text a lil bit

--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="preloader absolute bg-white left-0 top-0 w-screen h-screen overflow-hidden">
+    <main class="preloader absolute bg-white left-0 top-0 w-screen h-screen overflow-hidden flex items-center justify-center">
         loading...
     </main>
 </template>
